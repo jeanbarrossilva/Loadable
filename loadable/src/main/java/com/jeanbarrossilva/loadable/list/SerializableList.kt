bbfb989404962fafe4c0b0107a55a3328e246ca4
Jean Silva <jeanbarrossilva@outlook.com>
2023-04-14 14:08:40 -0300

    change: inline SerializableList

--- loadable/src/main/java/com/jeanbarrossilva/loadable/list/SerializableList.kt
+++ loadable/src/main/java/com/jeanbarrossilva/loadable/list/SerializableList.kt
@@ -2,5 +2,11 @@ package com.jeanbarrossilva.loadable.list
 
 import java.io.Serializable
 
-data class SerializableList<T> internal constructor(private val elements: List<T>) :
+/**
+ * [List] that conforms to [Serializable].
+ *
+ * @param elements Instances of [T] contained in this [SerializableList].
+ **/
+@JvmInline
+value class SerializableList<T> internal constructor(private val elements: List<T>) :
     List<T> by elements, Serializable
