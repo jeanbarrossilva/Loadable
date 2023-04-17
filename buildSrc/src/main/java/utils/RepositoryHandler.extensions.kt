383fc811d85542830eef391c91b5e1e197643335
Jean Silva <jeanbarrossilva@outlook.com>
2023-04-17 09:33:42 -0300

    fix(build): Maven repo URL

--- buildSrc/src/main/java/utils/RepositoryHandler.extensions.kt
+++ buildSrc/src/main/java/utils/RepositoryHandler.extensions.kt
@@ -5,7 +5,7 @@ import org.gradle.api.artifacts.repositories.MavenArtifactRepository
 /** Adds the repository in which Loadable is located. **/
 fun RepositoryHandler.loadable(): MavenArtifactRepository {
     return maven {
-        url = URI.create("https://maven.pkg.github.com/jeanbarrossilva/loadable-android")
+        url = URI.create("https://maven.pkg.github.com/jeanbarrossilva/loadable")
 
         credentials {
             username = System.getenv("GITHUB_USERNAME")
