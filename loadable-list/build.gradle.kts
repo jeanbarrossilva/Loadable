012c7d1564dc4426a953693492ccb18d120a18c3
Jean Silva <jeanbarrossilva@outlook.com>
2023-06-06 16:20:44 -0300

    refact: set up publishing for :loadable-list

--- loadable-list/build.gradle.kts
+++ loadable-list/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
+    `maven-publish`
 }
 
 java {
@@ -11,4 +12,22 @@ dependencies {
     api(project(":loadable"))
 
     testImplementation(Libraries.KOTLIN_TEST)
+}
+
+publishing {
+    repositories {
+        loadable()
+    }
+
+    publications {
+        register<MavenPublication>(Variants.RELEASE) {
+            groupId = Metadata.GROUP
+            artifactId = Metadata.artifact("list")
+            version = Versions.Loadable.NAME
+
+            afterEvaluate {
+                from(components["java"])
+            }
+        }
+    }
 }
