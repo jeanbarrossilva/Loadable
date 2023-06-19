36bbe088f65928d81551a3c586604ff95f695462
Jean Silva <jeanbarrossilva@outlook.com>
2023-06-19 16:54:51 -0300

    change: export :loadable-placeholder's sources and Javadoc

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -15,8 +15,8 @@ object Versions {
     val java = JavaVersion.VERSION_11
 
     object Loadable {
-        const val CODE = 13
-        const val NAME = "1.6.0"
+        const val CODE = 14
+        const val NAME = "1.6.1"
         const val SDK_COMPILE = 33
         const val SDK_MIN = 21
         const val SDK_TARGET = SDK_COMPILE
