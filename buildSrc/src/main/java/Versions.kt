276bd2b5cc91594f9b107ff497d11ad89426a3b6
Jean Silva <jeanbarrossilva@outlook.com>
2023-08-27 08:08:18 -0300

    change: add Flow#listLoadable extension for collections

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -16,8 +16,8 @@ object Versions {
     val java = JavaVersion.VERSION_11
 
     object Loadable {
-        const val CODE = 21
-        const val NAME = "1.6.8"
+        const val CODE = 22
+        const val NAME = "1.6.9"
         const val SDK_COMPILE = 33
         const val SDK_MIN = 21
         const val SDK_TARGET = SDK_COMPILE
